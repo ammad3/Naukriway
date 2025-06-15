f3e85279aef234163a291443b5060561301587bb
ammad3 <e.ammad@outlook.com>
2025-06-15 16:02:44 +0500

    navbar mobile responsive, modernize Login, signup page and few changes in input style

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  const menuBtn = document.getElementById('menu-btn');
+  const navLinks = document.getElementById('nav-links');
+
+  menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+
+console.log("script running")
